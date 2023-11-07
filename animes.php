170916dac210cb4945601bacb01240ce7e6c0024
AryssonLuz <arysson2020@gmail.com>
2023-11-07 08:27:29 -0300

    Update animes.php

--- animes.php
+++ animes.php
@@ -2,8 +2,7 @@
     
 Anime, animê ou animé, se refere é uma animação desenhada à mão ou por computação gráfica do Japão. Fora do Japão e em inglês, anime refere-se à animação japonesa e refere-se especificamente à animação produzida no Japão.
 
-
-
+ 
 </h3>
 
 <img src="https://w.forfun.com/fetch/89/898282fa13d69f3c3700ff76595f4e40.jpeg" alt="animes" style="display: flex; justify-content:center">
